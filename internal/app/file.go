bb34d0aefd3fa73b54e237c29bdef57c9928d091
Dmitry Osetrov <osetrovdv0x12@gmail.com>
2025-05-10 18:56:59 +0300

    Write logic for extracting number from file name

--- internal/app/file.go
+++ internal/app/file.go
@@ -1,6 +1,11 @@
 package app
 
-import "regexp"
+import (
+	"errors"
+	"fmt"
+	"regexp"
+	"strconv"
+)
 
 var nameRegex = regexp.MustCompile(`^(?:\d+\.\s)?(.+)$`)
 
@@ -13,6 +18,19 @@ func ExtractFileName(val string) string {
 	return ""
 }
 
-func ExtractNumber(val string) int64 {
-	return 0
+var numRegex = regexp.MustCompile(`^(\d+)\.\s`)
+var ErrWrongFormat = errors.New("string is in wrong format")
+
+func ExtractNumber(val string) (int64, error) {
+	matches := numRegex.FindStringSubmatch(val)
+	if len(matches) > 1 {
+		num, err := strconv.ParseInt(matches[1], 0, 64)
+		if err != nil {
+			return 0, fmt.Errorf("failed to convert extracted number %v to int64: %w", matches[1], err)
+		}
+
+		return num, nil
+	}
+
+	return 0, ErrWrongFormat
 }
