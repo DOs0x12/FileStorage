1f18a47c1cc65e3cf65932ffbd31386ede1e3cce
Dmitry Osetrov <osetrovdv0x12@gmail.com>
2025-05-17 19:49:34 +0300

    Add func of getting all file references to storage interface

--- internal/interfaces/storage/reference.go
+++ internal/interfaces/storage/reference.go
@@ -5,4 +5,5 @@ import "context"
 type ReferenceStorage interface {
 	Insert(ctx context.Context, num int64, ref string) error
 	GetReference(ctx context.Context, num int64) (string, error)
+	GetAllReferences(ctx context.Context) ([]string, error)
 }
